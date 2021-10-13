722f13e7383295b1b4eed71ab15b7a3d9c58700d
HoshmandG90 <hoshmandG900@gmail.com>
2021-10-13 15:55:24 +0300

    added table

--- app/Http/Controllers/Admin/Table.php
+++ app/Http/Controllers/Admin/Table.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use Livewire\Component;
-
+use Livewire\WithPagination;
+use App\models\user;
 class Table extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('admin.table')->extends('layouts.master');
+        $Latest=User::latest()->paginate(20);
+        return view('admin.table',compact('Latest'))->extends('layouts.master');
     }
 }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,6 +25,8 @@
    
     @livewireScripts
     @livewireStyles
+
+
     
     <style>
         ::-webkit-scrollbar {
